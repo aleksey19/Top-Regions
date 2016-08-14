cc578e10bdc6aba6f46ba5ac3ac3fb3b8627409a
Aleksey <poveredbyme@gmail.com>
2016-08-14 12:29:18 +0300

    First commit to remote

--- Top Regions/Model/Entities/Photo.m
+++ Top Regions/Model/Entities/Photo.m
@@ -15,7 +15,16 @@
 + (EKManagedObjectMapping *)objectMapping
 {
     return [EKManagedObjectMapping mappingForEntityName:@"Photo" withBlock:^(EKManagedObjectMapping *mapping) {
-        [mapping mapKeyPath:@"" toProperty:@""];
+        [mapping mapKeyPath:@"dateupload" toProperty:@"date_upload"];
+        [mapping mapKeyPath:@"description.content" toProperty:@"description"];
+        [mapping mapKeyPath:@"id" toProperty:@"photo_id"];
+        [mapping mapKeyPath:@"latitude" toProperty:@"latitude"];
+        [mapping mapKeyPath:@"longitude" toProperty:@"longitude"];
+        [mapping mapKeyPath:@"owner" toProperty:@"owner_id"];
+        [mapping mapKeyPath:@"ownername" toProperty:@"owner_name"];
+        [mapping mapKeyPath:@"place_id" toProperty:@"place_id"];
+        [mapping mapKeyPath:@"title" toProperty:@"title"];
+        [mapping mapKeyPath:@"woeid" toProperty:@"woe_id"];
     }];
 }
 
@@ -23,6 +32,23 @@
         inManagedObjectContext:(NSManagedObjectContext *)context
 {
     Photo *photo = nil;
+
+    NSString *photo_id = dictionary[@"id"];
+    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Photo"];
+    request.predicate = [NSPredicate predicateWithFormat:@"photo_id = %@", photo_id];
+    
+    NSError *error = nil;
+    NSArray *photos = [context executeFetchRequest:request error:&error];
+    
+    if (error || (photos.count > 1) || !photos) {
+#warning handle
+    }
+    else if (photos.count == 1) {
+        photo = [photos lastObject];
+    }
+    else if (!photos.count) {
+        photo = [Photo objectWithProperties:dictionary inContext:context];
+    }
     
     return photo;
 }
@@ -30,7 +56,9 @@
 + (void)loadPhotosFromArray:(NSArray *)array
    intoManagedObjectContext:(NSManagedObjectContext *)context
 {
-    
+    for (NSDictionary *photo in array) {
+        [self photoWithDictionary:photo inManagedObjectContext:context];
+    }
 }
 
 @end
